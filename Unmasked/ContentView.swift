252b6439537906f69ba7b876ae618f5703ae5e72
Natasha Radika <81007696+tashagrc@users.noreply.github.com>
2024-06-26 14:58:22 +0700

    Initial Commit

---
+++ Unmasked/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unmasked
+//
+//  Created by Natasha Radika on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Unmasked/UnmaskedApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnmaskedApp.swift
+//  Unmasked
+//
+//  Created by Natasha Radika on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct UnmaskedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
